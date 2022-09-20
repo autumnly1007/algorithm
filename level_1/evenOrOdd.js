5cfbb0bf299944a8c0f079ecba805ac0e8fca219
autumnly1007 <autumnly1007@gmail.com>
2022-09-20 22:25:21 +0900

    evenOrOdd
    짝수와 홀수

---
+++ level_1/evenOrOdd.js
@@ -0,0 +1,14 @@
+function solution(num) {
+  var answer = '';
+
+  if (num % 2 === 0) {
+    answer = 'Even';
+  } else {
+    answer = 'Odd';
+  }
+
+  return answer;
+}
+
+console.log(solution(1)); // "Odd"
+console.log(solution(2)); // "Even"
