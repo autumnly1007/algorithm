70802d52758fcd51ad7cb81c623b5f57f820e595
autumnly1007 <autumnly1007@gmail.com>
2022-10-06 09:21:37 +0900

    가운데 글자 가져오기

---
+++ level_1/가운데 글자 가져오기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  return s.length % 2 === 0
+    ? s.substr(s.length / 2 - 1, 2)
+    : s.substr(Math.floor(s.length / 2), 1);
+}
+
+console.log(solution('abcde')); // "c"
+console.log(solution('qwer')); // "we"
