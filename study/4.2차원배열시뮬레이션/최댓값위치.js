7534242007e90143f64beef6ac655eaf3f4b6aa1
autumnly1007 <autumnly1007@gmail.com>
2022-11-05 14:03:32 +0900

    4.2차원배열시뮬레이션

---
+++ study/4.2차원배열시뮬레이션/최댓값위치.js
@@ -0,0 +1,34 @@
+function solution(nums) {
+  let answer = [];
+  let max = 0;
+  let maxX = 0;
+  let maxY = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums[i].length; j++) {
+      if (nums[i][j] > max) {
+        max = nums[i][j];
+        maxX = i;
+        maxY = j;
+      }
+    }
+  }
+
+  answer.push(maxX + 1);
+  answer.push(maxY + 1);
+  return answer;
+}
+
+console.log(
+  solution([
+    [3, 23, 85, 34, 17, 74, 25, 52, 65],
+    [10, 7, 39, 42, 88, 52, 14, 72, 63],
+    [87, 42, 18, 78, 53, 45, 18, 84, 53],
+    [34, 28, 64, 85, 12, 16, 75, 36, 55],
+    [21, 77, 45, 35, 28, 75, 90, 76, 1],
+    [25, 87, 65, 15, 28, 11, 37, 28, 74],
+    [65, 27, 75, 41, 7, 89, 78, 64, 39],
+    [47, 47, 70, 45, 23, 65, 3, 41, 44],
+    [87, 13, 82, 38, 31, 12, 29, 29, 80],
+  ])
+); // [5, 7]
