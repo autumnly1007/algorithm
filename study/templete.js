2aabcfe5f886e35a65a8cd5bf874c150c1e8f447
autumnly1007 <autumnly1007@gmail.com>
2022-11-18 19:14:58 +0900

    Update templete.js

--- study/templete.js
+++ study/templete.js
@@ -1,4 +1,13 @@
 function solution() {
+  let answer = 0;
+  return answer;
+}
+
+console.log(solution());
+
+/////////////////////////////////
+
+function solution() {
   let answer = '';
   return answer;
 }
