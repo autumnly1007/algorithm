bb97f6aaf8ce464a745519cac44013cbaf4fe545
autumnly1007 <autumnly1007@gmail.com>
2022-11-29 20:39:45 +0900

    푸드 파이트 대회

--- level_1/푸드 파이트 대회.js
+++ level_1/푸드 파이트 대회.js
@@ -12,4 +12,5 @@ function solution(food) {
   return answer.join('');
 }
 
-console.log(solution());
+console.log(solution([1, 3, 4, 6])); // 1223330333221
+console.log(solution([1, 7, 1, 2])); // 111303111
