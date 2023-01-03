780f2dcf8a624edc98d577d583d43277cf274084
autumnly1007 <autumnly1007@gmail.com>
2023-01-03 21:24:30 +0900

    햄버거 만들기

---
+++ level_1/햄버거 만들기.js
@@ -0,0 +1,21 @@
+function solution(ingredient) {
+  let answer = 0;
+  const hamberger = '1231';
+  const stack = [];
+
+  ingredient.forEach((item, idx) => {
+    stack.push(item);
+
+    if (stack.length >= 4) {
+      if (stack.slice(-4).join('') === hamberger) {
+        answer++;
+        stack.splice(-4, 4);
+      }
+    }
+  });
+
+  return answer;
+}
+
+console.log(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])); // 2
+console.log(solution([1, 3, 2, 1, 2, 1, 3, 1, 2])); // 0
