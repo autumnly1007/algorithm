481027bf980b03c57af59a6ef3e57a25b0f60754
autumnly1007 <autumnly1007@gmail.com>
2023-01-19 15:27:15 +0900

    숫자의 표현

---
+++ level_2/숫자의 표현.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let count = 0;
+
+  for (let i = 1; i < n / 2 + 1; i++) {
+    const sum = (i * (i + 1)) / 2;
+    const sub = n - sum;
+    if (sub < 0) break;
+    if (sub % i === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(solution(15)); // 4
